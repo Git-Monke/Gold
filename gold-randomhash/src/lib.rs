5b73a5fc7cc2b8381eb197a4794a5cfea5e82c53
Git-Monke <javelasq2006@gmail.com>
2024-12-14 17:46:51 -0800

    Completed murmur3

--- gold-randomhash/src/lib.rs
+++ gold-randomhash/src/lib.rs
@@ -1,5 +1,65 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use ascon_hash::AsconHash;
+use belt_hash::BeltHash;
+use blake2::Blake2s256;
+use fsb::Fsb256;
+use gost94::Gost94CryptoPro;
+use groestl::Groestl256;
+use jh::Jh256;
+use ripemd::Ripemd256;
+use sha1_checked::Sha1;
+use sha2::Sha256;
+use sha3::Sha3_256;
+use shabal::Shabal256;
+use skein::{consts::U32, Skein256};
+use sm3::Sm3;
+use streebog::Streebog256;
+use tiger::Tiger2;
+use whirlpool::Whirlpool;
+
+use digest::{Digest, DynDigest};
+
+mod murmur3;
+
+struct RandomHash {
+    algs: Vec<Box<dyn DynDigest>>,
+    n: usize,
+    m: usize,
+}
+
+impl RandomHash {
+    pub fn new() -> Self {
+        // Algs can continue to grow as long as the provided hash function impleents DynDigest!
+        // 18 algorithms is enough for now...
+        let algs: Vec<Box<dyn DynDigest>> = vec![
+            Box::new(AsconHash::new()),
+            Box::new(BeltHash::new()),
+            Box::new(Blake2s256::new()),
+            Box::new(Fsb256::new()),
+            Box::new(Gost94CryptoPro::new()),
+            Box::new(Groestl256::new()),
+            Box::new(Jh256::new()),
+            Box::new(Ripemd256::new()),
+            Box::new(Sha1::new()),
+            Box::new(Sha256::new()),
+            Box::new(Sha3_256::new()),
+            Box::new(Shabal256::new()),
+            Box::new(Skein256::<U32>::new()),
+            Box::new(Sm3::new()),
+            Box::new(Streebog256::new()),
+            Box::new(Tiger2::new()),
+            Box::new(Whirlpool::new()),
+        ];
+
+        RandomHash {
+            algs,
+            n: 5,
+            m: 10_000,
+        }
+    }
+}
+
+trait Checksum {
+    fn checksum(&self) -> &[u8];
 }
 
 #[cfg(test)]
@@ -7,8 +67,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_murmur3() {
+        assert_eq!(murmur3::murmur3_32(b"", 1), 1_364_076_727)
     }
 }
