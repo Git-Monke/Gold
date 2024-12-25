f04715b31497704ed2b1eccda48481b2256160e5
Git-Monke <javelasq2006@gmail.com>
2024-12-25 14:58:56 -0800

    Defined basic package layout

--- gold-structures/src/error.rs
+++ gold-structures/src/error.rs
@@ -1,2 +1,10 @@
-pub type Result<T> = core::result::Result<T, Error>;
-pub type Error = Box<dyn std::error::Error>;
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    // Generic
+    #[error("Generic {0}")]
+    Generic(String),
+
+    // Io
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
--- gold-structures/src/lib.rs
+++ gold-structures/src/lib.rs
@@ -1,7 +1,38 @@
-pub mod block;
-pub mod byte_reader;
-pub mod compact;
-pub mod error;
-pub mod tx;
+use secp256k1::{ecdsa::Signature, PublicKey};
 
-pub use error::{Error, Result};
+mod error;
+mod prelude;
+mod utils;
+
+// These aliases are so the code can describe itself. They get optimized away at compile time.
+type HashAsBytes = [u8; 32];
+type Txid = [u8; 32];
+
+pub struct Block {
+    header: Header,
+    tx_list: Vec<Transaction>,
+}
+
+pub struct Header {
+    prev_block_hash: HashAsBytes,
+    timestamp: u32,
+    merkle_root: HashAsBytes,
+    nonce: u32,
+}
+
+pub struct Transaction {
+    inputs: Vec<TxInput>,
+    outputs: Vec<TxOutput>,
+}
+
+pub struct TxInput {
+    txid: Txid,
+    index: u8,
+    sigs: Vec<Signature>,
+}
+
+pub struct TxOutput {
+    amount: usize,
+    keys: Vec<PublicKey>,
+    sig_requirement: usize,
+}
